cbfa46ec7e8d22fd5e43aa267431a94b2f785db0
rchurunel-2026252@kinal.edu.gt <rchurunel-2026252@kinal.edu.gt>
2026-08-23 18:07:42 -0600

    Add files via upload

---
+++ audit-user-product/src/org/randychurunel/system/ClasePrincipal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.randychurunel.system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.randychurunel.system.utils.SceneManager;
+import org.randychurunel.system.utils.ViewFactory;
+
+
+
+public class ClasePrincipal extends Application{
+ 
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stageRoot){
+        SceneManager.getInstanciaSceneManager().setStagePrincipal(stageRoot);
+        ViewFactory viewFacto = new ViewFactory();
+        viewFacto.viewLogin();
+    }
+}
---
+++ audit-user-product/src/org/randychurunel/system/repository/UserInteface.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.randychurunel.system.repository;
+
+import org.randychurunel.system.model.User;
+
+public class UserInteface {
+    public void create(User user){};
+}
